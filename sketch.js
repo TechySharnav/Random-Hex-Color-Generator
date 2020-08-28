723380a5cb229f51ae8ef26e64b620cf2895518f
Sharnav M <63995097+TechySharnav@users.noreply.github.com>
2020-08-28 16:00:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const span = document.getElementById("Color");
 const body = document.querySelector("body");
 const btn = document.getElementById("btn");
-
+const hexC = document.getElementById("hexC");
 
 function generateHex() {
     var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c', 'd', 'e', 'f']
@@ -17,15 +17,16 @@ function generateHex() {
 
     var Color = tinycolor(color);
     hsl = Color.toHsl();
+
     if (hsl["l"] > 0.5) {
         span.style.color = "black";
         btn.style.color = "black";
-        span.style.textDecorationColor = "black";
+        hexC.style.textDecorationColor = "black";
         btn.style.borderColor = "black";
     } else {
         span.style.color = "white";
         btn.style.color = "white";
-        span.style.textDecorationColor = "white";
+        hexC.style.textDecorationColor = "white";
         btn.style.borderColor = "white"
     }
 }
