50d1bf837f2c71fd4cee92e04547219bbd606938
Sharnav M <63995097+TechySharnav@users.noreply.github.com>
2020-08-28 15:47:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 const span = document.getElementById("Color");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
+const btn = document.getElementById("btn");
 
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c', 'd', 'e', 'f']
 
 function generateHex() {
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c', 'd', 'e', 'f']
     var count = 0;
     var color = '#'
     while (count < 6) {
@@ -13,4 +14,18 @@ function generateHex() {
 
     span.innerHTML = color;
     body.style.background = color;
+
+    var Color = tinycolor(color);
+    hsl = Color.toHsl();
+    if (hsl["l"] > 0.5) {
+        span.style.color = "black";
+        btn.style.color = "black";
+        span.style.textDecorationColor = "black";
+        btn.style.borderColor = "black";
+    } else {
+        span.style.color = "white";
+        btn.style.color = "white";
+        span.style.textDecorationColor = "white";
+        btn.style.borderColor = "white"
+    }
 }
